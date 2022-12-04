6f4ad595784ba1a75dcee98ec5c9ff77b6c56310
altynovbayel <altynovich30@gmail.com>
2022-12-05 00:26:10 +0600

    update userDetails

--- src/pages/UserDetails/index.jsx
+++ src/pages/UserDetails/index.jsx
@@ -1,32 +1,59 @@
 import React from 'react'
-import Header from '../../components/Header'
+import './UserDetails.scss'
 import Repositories from '../../components/Repositories'
 import SearchRepo from '../../components/SearchRepo'
 import UserProfile from "../../components/UserProfile";
 import {api} from "../../config/api";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai";
 
 function UserDetails({login}) {
   const [user, setUser] = React.useState('')
   const [repos, setRepos] = React.useState('')
+  const [currentArr, setCurrentArr] = React.useState('')
+  
+  const len = 10
+  const [page, setPage] = React.useState(1);
+  const pageCount = Math.ceil(repos.length / len)
+  const lastContentIndex = page * len;
+  const firstContentIndex = lastContentIndex - len;
   
   React.useEffect(() => {
     api.getUser(login).then(r => r && setUser(r.data))
     api.getRepos(login).then(r => r && setRepos(r.data))
   }, [login])
   
-  if(!user || !repos) return <h1>loading</h1>
+  React.useEffect(() => {
+    setCurrentArr(repos.slice(firstContentIndex, lastContentIndex))
+  },[page, repos])
+  
+  if (!user || !repos) return <h1>loading</h1>
   return (
     <div className='main'>
       <div className="main__row">
         <UserProfile {...user} />
         <div className="main__repos">
-          <SearchRepo repos={repos} />
+          <SearchRepo repos={repos}/>
           {
-          repos &&
-          repos.map((item, id) => {
-            return <Repositories key={id} {...item} pub={item.private} />
-          })
+            currentArr &&
+            currentArr.map((item, id) => {
+              return <Repositories key={id} {...item} pub={item.private}/>
+            })
           }
+          <div className='pagination'>
+            <button onClick={e => {
+              e.preventDefault()
+              page > 1 && setPage(prev => prev - 1)
+            }}>
+              <AiOutlineArrowLeft/>
+            </button>
+            {page} / {pageCount}
+            <button onClick={e => {
+              e.preventDefault()
+              page < pageCount && setPage(prev => prev + 1)
+            }}>
+              <AiOutlineArrowRight/>
+            </button>
+          </div>
         </div>
       </div>
     </div>
