83d3b65580fea33cf766367a1127509e78ab4d36
altynovbayel <altynovich30@gmail.com>
2022-12-05 00:15:17 +0600

    fixed axios request

--- src/config/api.js
+++ src/config/api.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export const api = {
   getUser: (username) => axios.get(`/users/${username}`),
   getRepos: (username) => axios.get(`/users/${username}/repos`),
-  reposFilter: (username, type, lang, sort) => axios.get(`/users/${username}?q=&sort=${sort}`),
-  findUser: (query) => axios.get(`/search/repositories?q=${query}{&page,per_page,sort,order}`)
+  reposFilter: (username, type, lang, sort) => axios.get(`/users/${username}?q=sort=${sort}`),
+  findUser: (username) => axios.get(`/search/users?q=${username}`),
 }
